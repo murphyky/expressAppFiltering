2d10775cc5d70c9d7fe60d92dde1d34e75889074
kyle <kyle-murphy.co.uk>
2018-04-14 12:49:35 +0100

    backup

--- index.js
+++ index.js
@@ -16,9 +16,12 @@ MongoClient.connect('mongodb://resFilterUser:km890889@localhost:27017/', (err,
 
     app.use(cors())
 
+    app.use(bodyParser.json());
+
+    /*
     app.use(bodyParser.urlencoded({
         extended: true
-    }));
+    }));*/
 
     app.set('view engine', 'ejs');
 
