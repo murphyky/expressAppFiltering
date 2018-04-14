ed8c066eee50a7f7caaf621a2da4d194bf6de737
kyle <kyle-murphy.co.uk>
2018-04-14 12:55:30 +0100

    backup

--- index.js
+++ index.js
@@ -16,6 +16,10 @@ MongoClient.connect('mongodb://resFilterUser:km890889@localhost:27017/', (err,
 
     app.use(cors())
 
+    app.use(bodyParser.json({
+        type: "application/json"
+    }))
+
     /*
     app.use(bodyParser.urlencoded({
         extended: true
